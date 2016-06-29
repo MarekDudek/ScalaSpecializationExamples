2f40ec9bd2413e4e3d5da7ded063b8577b2688bf
Marek Dudek <marek.dudek@gmail.com>
2016-06-29 23:31:18 +0200

    water-pouring problem

---
+++ src/test/scala/com/marekdudek/WaterPouringProblemSpec.scala
@@ -0,0 +1,24 @@
+package com.marekdudek
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class WaterPouringProblemSpec extends FlatSpec with Matchers {
+
+  "water pouring problem" should "be solved" in {
+    // given
+    val wpp = new WaterPouringProblem(Vector(4, 9))
+    // when
+    val solution = wpp.solution(6).head
+    // then
+    solution shouldBe new wpp.Path(List(
+      wpp.Fill(1),
+      wpp.Pour(1, 0),
+      wpp.Empty(0),
+      wpp.Pour(1, 0),
+      wpp.Empty(0),
+      wpp.Pour(1, 0),
+      wpp.Fill(1),
+      wpp.Pour(1, 0)
+    ).reverse)
+  }
+}
