5ed21c724af15036f51057a0111cffdcf59aa4b0
Marek Dudek <marek.dudek@gmail.com>
2016-07-09 10:54:10 +0200

    reformatted for clarity

--- src/test/scala/com/marekdudek/WaterPouringProblemSpec.scala
+++ src/test/scala/com/marekdudek/WaterPouringProblemSpec.scala
@@ -19,6 +19,7 @@ class WaterPouringProblemSpec extends FlatSpec with Matchers {
       wpp.Pour(1, 0),
       wpp.Fill(1),
       wpp.Pour(1, 0)
-    ).reverse, Vector(4, 6).asInstanceOf[WaterPouringProblem#State])
+    ).reverse,
+      Vector(4, 6).asInstanceOf[WaterPouringProblem#State])
   }
 }
